141addd677bd4b184f7a599fc7d40b497bd288e1
Filip Janko <fikoun@icloud.com>
2019-03-27 19:34:49 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	require 'SMTP.php';
+	require 'PHPMailer.php';
+
+	if (isset($_POST['submit']) &&
+		isset($_POST['to']) &&
+		isset($_POST['subject']) && 
+		isset($_POST['message']))
+	{
+		$mail = new PHPMailer(); // create a new object
+		$mail->IsSMTP(); // enable SMTP
+		$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+		$mail->SMTPAuth = true; // authentication enabled
+		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+		$mail->Host = "smtp.mailtrap.io";
+		$mail->Port = 465; 
+		$mail->IsHTML(true);
+		$mail->Username = "";
+		$mail->Password = "";
+		$mail->SetFrom("");
+		$mail->Subject = $_POST['subject'];
+		$mail->Body = $_POST['message'];
+		$mail->AddAddress($_POST['to']); 
+		
+		$mail = true; // $mail->Send();
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mail</title>
+	<style>
+		body{
+			text-align: center;
+		}
+		.wrap{
+			padding: 20px;
+			display: inline-block;
+		}
+		.wrap textarea{
+			widows: 100%;
+			margin: 10px 0;
+		}
+	</style>
+</head>
+<body>
+	<h3 class="wrap">
+		<p> 
+			<?php 
+			 	if(isset($mail) && !$mail)
+			 		echo "Error";
+				else if(isset($mail) && $mail)
+			 		echo "Message has been sent";
+			 ?>	
+		 </p>
+		<form action="./index.php" method="post">
+			Příjemce: <input name="to" type="mail"> <br>
+			Předmět: <input name="subject" type="text"> <br>
+			<textarea cols="32" rows="10" name="message"></textarea> <br>
+			<input name="submit" type="submit" value="Poslat">
+		</form>
+	</h3>
+</body>
+</html>
